3ddea1b15dcd76535ab0644cbef4bb801fdce77a
20k32 <egormobtel@gmail.com>
2023-03-27 21:42:21 +0300

    added logic without realization

--- FabricMethodCaffee/MainWindow.xaml.cs
+++ FabricMethodCaffee/MainWindow.xaml.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Windows.Controls;
 using System.Linq;
 using ChainOfResponsibility_Servers;
+using System.Globalization;
+using System.Windows.Data;
 
 namespace FabricMethodCaffee
 {
@@ -12,6 +14,31 @@ namespace FabricMethodCaffee
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     /// 
+
+    public class DoubleToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double d)
+            {
+                return (int)Math.Round(d);
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    // чтоб переслать сообщение нужно: текст сообщения, число секунд задерки.
+    // чтоб получить сообщение нужно: хранить кэш всех сообщений между пользователями
+
+    // у каждого пользователя будет свой кэш, пользователи загружаються в программу заранее.
+    // пользователь может отправить остальным пользователям сообщение, для этого у пользователя должна быть
+    // коллекция других пользователей
+
     public partial class MainWindow : Window
     {
         private List<string> currentUsers = null!;
@@ -37,6 +64,8 @@ namespace FabricMethodCaffee
             {
                 SendToBox.Items.Add(user);
             }
+
+            SendToBox.SelectedIndex = 0;
 
             switch (((ComboBox)sender).SelectedIndex)
             {
